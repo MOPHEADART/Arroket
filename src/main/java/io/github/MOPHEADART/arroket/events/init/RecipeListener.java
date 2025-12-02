706e031dbffdc21ae8c05ddea882bc168a7b2de5
MOPHEADART <adamrhinehart@proton.me>
2025-12-01 20:55:23 -0500

    Add RecipeListener

--- src/main/java/io/github/MOPHEADART/arroket/events/init/RecipeListener.java
+++ src/main/java/io/github/MOPHEADART/arroket/events/init/RecipeListener.java
@@ -1,14 +1,13 @@
 package io.github.MOPHEADART.arroket.events.init;
 
 import net.mine_diver.unsafeevents.listener.EventListener;
-import net.modificationstation.stationapi.api.event.mod.InitEvent;
+import net.modificationstation.stationapi.api.event.recipe.RecipeRegisterEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.EntrypointManager;
 import net.modificationstation.stationapi.api.util.Namespace;
-import org.apache.logging.log4j.Logger;
 
 import java.lang.invoke.MethodHandles;
 
-public class InitListener {
+public class RecipeListener {
     static {
         EntrypointManager.registerLookup(MethodHandles.lookup());
     }
@@ -16,10 +15,8 @@ public class InitListener {
     @SuppressWarnings("UnstableApiUsage")
     public static final Namespace NAMESPACE = Namespace.resolve();
 
-    public static final Logger LOGGER = NAMESPACE.getLogger();
-
     @EventListener
-    private static void serverInit(InitEvent event) {
-        LOGGER.info(NAMESPACE.toString());
+    private static void registerRecipes(RecipeRegisterEvent event) {
+        RecipeRegisterEvent.Vanilla type = RecipeRegisterEvent.Vanilla.fromType(event.recipeId);
     }
 }
